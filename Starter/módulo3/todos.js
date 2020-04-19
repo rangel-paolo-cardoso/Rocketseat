adf94eaa0f6c8496f176011d3e0633c6753791ac
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-19 16:10:29 -0300

    Implementada função que adiciona um novo todo na lista.

--- Starter/módulo3/todos.js
+++ Starter/módulo3/todos.js
@@ -9,6 +9,8 @@ let todos = [
 ];
 
 function renderTodos() {
+    listElement.innerHTML = '';
+    
     for (todo of todos) {
         let todoElement = document.createElement('li');
         let todoText = document.createTextNode(todo);
@@ -19,3 +21,13 @@ function renderTodos() {
 }
 
 renderTodos();
+
+function addTodo() {
+    let todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
