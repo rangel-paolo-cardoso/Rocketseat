983ad72a4cb4ada55526d9705a5922f53bf0e905
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-03 00:33:21 -0300

    exercício 4 de desustruturação finalizado.

--- Starter/JavaScript-ES6/módulo1/ex4/main.js
+++ Starter/JavaScript-ES6/módulo1/ex4/main.js
@@ -1,3 +1,4 @@
+//4.1
 const empresa = {
   nome: 'Rocketseat',
   endereco: {
@@ -6,8 +7,18 @@ const empresa = {
   },
 };
 
-const { nome, endereco: { cidade, estado } } = empresa;
+const {
+  nome,
+  endereco: { cidade, estado },
+} = empresa;
 
 console.log(nome);
 console.log(cidade);
 console.log(estado);
+
+//4.2
+function mostraInfo({ nome, idade }) {
+  return `${nome} tem ${idade} anos.`;
+}
+
+console.log(mostraInfo({ nome: 'Diego', idade: 23 }));
